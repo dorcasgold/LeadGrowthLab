8c310465ec58abd998b6519d3e1120bf43013e29
odetayo dorcas <odetayodorcas123@gmail.com>
2024-06-29 06:29:38 +0100

    feat: finish SecondCard section

--- src/Home/SecondCard.jsx
+++ src/Home/SecondCard.jsx
@@ -7,17 +7,17 @@ import Analytics from '../assets/Analytics-Image.svg'
 function SecondCard() {
   return (
     <div className="mt-28">
-      <div className="px-5 mx-10 my-20 text-white flex items-center gap-16">
+      <div className="md:px-5 px-2 mx-5 my-20 text-white flex-col flex items-center gap-16 lg:flex-row">
         <div>
           <div className="flex flex-col gap-3 top">
             <div>
-              <span className="text-green-500 underline archivo text-5xl">Turbocharge your</span>
+              <span className="text-green-500 underline archivo text-2xl md:text-5xl">Turbocharge your</span>
+            </div>
+            <div className='flex gap-3'>
+              <span className="text-green-500 underline archivo text-2xl md:text-5xl">marketing</span> <span className="text-2xl md:text-5xl archivo">with</span>
             </div>
             <div>
-              <span className="text-green-500 underline archivo text-5xl">marketing</span> <span className="text-5xl archivo">with</span>
-            </div>
-            <div>
-              <p className="text-white archivo text-5xl">SalesInsight&apos;s tools</p>
+              <p className="text-white archivo text-xl md:text-5xl">SalesInsight&apos;s tools</p>
             </div>
             <div className="flex flex-col">
               <span>Boomslang offers powerful marketing tools to boost sales and leads.</span>
@@ -25,20 +25,20 @@ function SecondCard() {
             </div>
           </div>
 
-          <div className='grid grid-cols-2 gap-x-1 gap-y-5'>
-            <div className=' flex w-52 dark gap-3 items-center py-3 px-1 justify-center rounded-lg'>
+          <div className='grid lg:grid-cols-2 gap-x-3 gap-y-5 grid-cols-1 my-10'>
+            <div className=' flex dark gap-3 items-center py-3 px-2 justify-start rounded-lg'>
               <img src={Rocket} alt="icon" className='w-14 light px-2 rounded-xl' />
               <p className='archivo'>Boost Sales</p>
             </div>
-            <div className='flex w-52 dark gap-3 items-center py-3 px-1 justify-center rounded-lg'>
+            <div className='flex dark gap-3 items-center py-3 px-2 justify-start rounded-lg'>
               <img src={treeStructure} className='w-14 light px-2 rounded-xl' alt="icon" />
               <p className='archivo'>Capture Leads</p>
             </div>
-            <div className='flex w-52 dark gap-3 items-center py-3 px-1 justify-center rounded-lg'>
+            <div className='flex dark gap-3 items-center py-3 px-2 justify-start rounded-lg'>
               <img src={ChartBar} className='w-14 light px-2 rounded-xl' alt="icon" />
               <p className='archivo'>Data Analysis</p>
             </div>
-            <div className='flex w-52 dark gap-3 items-center py-3 px-1 justify-center rounded-lg'>
+            <div className='flex dark gap-3 items-center py-3 px-2 justify-start rounded-lg'>
               <img src={Lightbulb} className='w-14 light px-2 rounded-xl' alt="icon" />
               <p className='archivo'>Decision-Making</p>
             </div>
